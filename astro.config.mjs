e1b4afa3c4701470fa048e1ac23e2edddeeeed26
Justyna Klewicka <justyna.klewicka@outlook.com>
2025-11-18 14:35:21 +0100

    added hamburger menu for mobile

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,13 @@ import { defineConfig } from 'astro/config';
 
 export default defineConfig({
   publicDir: './public',
+  
+  server: {
+    host: true,        
+    port: 4321,        
+    strictPort: true,  
+  },
+  
   vite: {
     server: {
       fs: {
